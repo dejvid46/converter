d28c29a6a35ca54fa00b779648c3b1ee72bef042
daviddvorak <dejviddvorak146@gmail.com>
2025-11-04 16:38:43 +0100

    feat(types): added beather types

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,31 +4,32 @@ import configuration from './config/configuration';
 import path from 'path';
 import sharp from 'sharp';
 
+interface ReceivedMessage {
+  filename: string;
+  path: string;
+  targetFormat: string;
+}
+
 @Controller()
 export class ReceiverController {
 
   @MessagePattern(configuration().RMQPattern)
-  async handleMessage(data: Record<string, any>) {
+  async handleMessage(data: ReceivedMessage) {
     console.log('Received message:', data);
+
+    const inputPath = `${configuration().uploads}/${data.filename}`;
+    const outputFilename = path.basename(data.filename, path.extname(data.filename)) + "." + data.targetFormat;
+    const outputPath = `${configuration().converted}/${outputFilename}`;
+
     try {
-
-      console.log(data.path);
-
-      const inputPath = `${configuration().uploads}/${data.filename}`; // původní JPG soubor
-      const outputFilename = path.basename(data.filename, path.extname(data.filename)) + '.png';
-      const outputPath = `${configuration().converted}/${outputFilename}`;
-
-      // konverze pomocí sharp
       await sharp(inputPath)
         .png()
         .toFile(outputPath);
+      
+      let res = { status: 'done', output: outputFilename };
+      console.log(res);
 
-      // volitelně můžeš smazat původní JPG
-      // await fs.unlink(inputPath);
-
-      console.log(`Converted file saved at: ${outputPath}`);
       return { status: 'done', output: outputFilename };
-
     } catch (err) {
       console.error('Image conversion failed:', err);
       return { status: 'error', error: err.message };
--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,7 +1,7 @@
 export default () => ({
   port: parseInt(process.env.PORT || "3000", 10),
-  uploads: process.env.UPLOADS || "./uploads",
-  converted: process.env.CONVERTED || "./converted",
+  uploads: process.env.UPLOADS || "./storage/uploads",
+  converted: process.env.CONVERTED || "./storage/converted",
   RMQPattern: process.env.RMQPATTERN || "image_pattern",
   rabbitMQ_URL: process.env.RABBITMQ_URL || "amqp://localhost:5672",
   rabbitMQQueue: process.env.RABBITMQQUEUE || "image_convert"
